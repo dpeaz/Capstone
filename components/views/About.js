b3c02ebeb9a6ec4896462b15c9445af84d2505d2
Derek Pease <dcpeaz@gmail.com>
2023-12-28 18:24:59 -0500

    Converted to SPA

---
+++ components/views/About.js
@@ -0,0 +1,24 @@
+import html from "html-literal";
+() => html`HTML template literal`;
+
+export { default as About } from "./About";
+
+<h1>Bitcorns</h1>
+<img src="docs\media\004.png" class="bitcorn" />
+<h2>About Us</h2>
+<p>Bitcorns are 100 pixelated profile pictures inscribed as data on the bitcoin blockchain. Bitcorns
+  are among the first ever inscriptions on bitcoin, inscribed in the 39k-47k range. As of today,
+  there are over 45 million inscriptions on the bitcoin blockchain. The cheapest Bitcorn on the
+  market today is for sale for .032 bitcoin, or approximately $1,300 USD.
+</p>
+<h2>Our Vision</h2>
+<p>We believe that Bitcoin and Ordinals should (and can) be fun and profitable while fostering a
+  strong community where holders help one another instead of creating a PvP environment.</p>
+  <p>
+    We aim to achieve fun through creating funny, engaging content as well as producing sporadic,
+    meaningful Ordinals collections and community events.  We believe that remaining active daily
+    in the community, providing value, and treating people with respect and dignity is the best way
+    to build a strong community. While cryptocurrency is an extremely volatile market, one of our
+    primary goals is to generate sustainable revenue for the project in order to provide the best
+    opportunity for our holders to be profitable by investing in our project.
+  </p>
---
+++ components/views/Contact.js
@@ -0,0 +1,34 @@
+import html from "html-literal";
+() =>
+  html`
+    HTML template literal
+  `;
+
+export { default as Contact } from "./Contact";
+
+<div class="content-text">
+<h1>Contact Us</h1>
+<br>
+<a href="https://bitcorns.xyz">Official Website</a><br>
+<a href="https://twitter.com/ordinalbitcorns">Twitter</a>
+<br><br>
+<h2>Founder</h2>
+<br>
+<a href="https://twitter.com/dpeaz">dpeaz Twitter</a>
+</div></div>
+</body>
+</html>
+
+<h3>If you are interested in collaboration opportunities, please submit your request below:</h3>
+<form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/mrgwnwnw" method="post">
+  <fieldset id="fs-frm-inputs">
+    <label for="project">Project</label><br>
+    <input type="text" name="name" id="project" placeholder="Project you're representing" required=""><br>
+    <label for="email-address">Email Address</label><br>
+    <input type="email" name="_replyto" id="email-address" placeholder="name@gmail.com" required=""><br>
+    <label for="message">Message</label><br>
+    <textarea rows="5" name="message" id="message" placeholder="What is your collaboration proposal?" required=""></textarea><br>
+    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+  </fieldset>
+  <input type="submit" value="Submit">
+</form>
