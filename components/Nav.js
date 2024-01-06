a15592efc44efb4e125dafecba119901c0eadf2c
Derek Pease <dcpeaz@gmail.com>
2024-01-06 18:36:49 -0500

    Navbar an CSS clean up

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,12 @@
 import html from "html-literal";
 export default links => html`
-
   <nav class="navbar">
-    <a href="home.html"><img src="corn_kernel.png" alt="Bitcorns" width="50" height="50">
-    <a href="home.html">Home</a>
-    <a href="about.html">About</a>
-    <a href="contact.html">Contact</a>
-    <a href="bitmap.html">Bitmap</a>
+    ${links
+      .map(
+        link =>
+          `<a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a>`
+      )
+      .join("")}
 
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
@@ -17,9 +17,6 @@ export default links => html`
         )
         .join("")}
     </ul>
+    <ul class="hidden--mobile nav-links"></ul>
   </nav>
 `;
-
-{
-  /*  */
-}
