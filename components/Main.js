6b27f2377d3d4e25d7fdf5d3718c489a15c0d6c5
Derek Pease <dcpeaz@gmail.com>
2024-01-05 22:05:37 -0500

    SPA loading properly

--- components/Main.js
+++ components/Main.js
@@ -4,5 +4,5 @@ import html from "html-literal";
 import * as views from "./views";
 
 export default state => html`
-${views[state.view](state)}
+    ${views[state.view](state)}
   `;
--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,22 @@
 import html from "html-literal";
+export default links => html`
+  <nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
+      ${links
+        .map(
+          link =>
+            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+        )
+        .join("")}
+    </ul>
+  </nav>
+`;
 
-export default () => html`
-<nav class="nav">
+{
+  /* <nav class="nav">
     <a href="index.html"><img src="docs\media\corn_kernel.png" alt="Bitcorns" width="50" height="50">
     <a href="index.html">Home</a>
     <a href="about.html">About</a>
-    <a href="contact.html">Contact</a>
-</nav>`;
+    <a href="contact.html">Contact</a> */
+}
