0b030841786eedecc08911ecb506ae5dd527d844
Derek Pease <dcpeaz@gmail.com>
2024-01-07 16:12:15 -0500

    HTML and CSS updates

--- components/Nav.js
+++ components/Nav.js
@@ -6,14 +6,13 @@ export default links => html`
     <a href="/" title="Home">
       <img src="${cornKernel}" alt="" width="50em" height="50em" />
     </a>
+    ${links
+      .map(
+        link =>
+          `<a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a>`
+      )
+      .join("")}
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
-      ${links
-        .map(
-          link =>
-            `<a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a>`
-        )
-        .join("")}
-    </ul>
   </nav>
 `;
--- components/views/Bitmap.js
+++ components/views/Bitmap.js
@@ -7,12 +7,12 @@ export default () => html`
   <section>
     <div class="main-content">
   <h1>Bitmap Floor Price Check</h1>
-  <p>Enter Bitmap # or inscription ID in the search box. Results will show current Magic Eden
-  floor price for each attribute to give you a better idea of the value of your Bitmap. This
-  tool will help mitigate underpricing your listings.</p>
+  <p>Enter Bitmap # or inscription ID in the search box. Request will return current Magic Eden
+  floor price for each attribute of your request Bitmap inscription to provide more insight to the
+  value of your Bitmap. This tool will help mitigate underpricing your listings.</p>
   <form id="form" class="bitmap">
     <input type="search" id="bitmap" name="bitmap" placeholder="Bitmap # or Inscription ID">
-    <button>Search</button>
+    <button>Submit</button>
   </form>
   <img src="bitmap.webp" height="250px" width="250px" />
   <table>
