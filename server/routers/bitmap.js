573bd1e01a9fb91f3a239627fe890f808184d2a4
Derek Pease <dcpeaz@gmail.com>
2024-01-07 21:19:29 -0500

    Insomnia functioning properly

--- server/routers/bitmap.js
+++ server/routers/bitmap.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
-import Bitmap from "../models/Bitmap.js";
+import Bitmap from "../models/bitmap.js";
 
 const router = Router();
 
-// Create bitmap route
 router.post("/", async (request, response) => {
   try {
-    const newBitmapCheck = new BitmapCheck(request.body);
+    const newBitmap = new Bitmap(request.body);
 
-    const data = await newBitmapCheck.save();
+    const data = await newBitmap.save();
 
     response.json(data);
   } catch (error) {
@@ -21,5 +20,13 @@ router.post("/", async (request, response) => {
     return response.status(500).json(error.errors);
   }
 });
+
+async function fetchBitmapTokenInfo() {
+  const response = await fetch(
+    "https://api-mainnet.magiceden.io/v2/ord/btc/activities?limit=20&offset=0&kind[]=buying_broadcasted&kind[]=mint_broadcasted&kind[]=list&kind[]=delist&kind[]=create&kind[]=transfer&kind[]=offer_placed&kind[]=offer_cancelled&kind[]=offer_accepted_broadcasted&tokenId="
+  );
+  const data = await response.json();
+  return data;
+}
 
 export default router;
